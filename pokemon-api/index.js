7d8b9d22bb5100fc3402981b1de7d8402ee6d8d1
shaharKamay <shahar.kamay@gmail.com>
2021-10-21 16:48:35 +0300

    put everything inside pokemon-api folder

---
+++ pokemon-api/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+// start the server
+app.listen(port, function() {
+  console.log('app started');
+});
+
+// route our app
+app.get('/', function(req, res) {
+  res.send('hello world!');
+});
